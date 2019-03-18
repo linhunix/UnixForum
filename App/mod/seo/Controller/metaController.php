9775ae7d28d45c62cc9c8fd4f71b7bd64c33e4bf
LinHUniX Andrea Morello <andrea.morello@unix.team>
2019-03-18 06:20:03 +0000

    Fix Change about UnixFans

--- App/mod/seo/Controller/metaController.php
+++ App/mod/seo/Controller/metaController.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * 
+ *
  */
-namespace UecForum\seo\Controller;
+namespace App\seo\Controller;
 use LinHUniX\Mcp\Model\mcpBaseModelClass;
 
 class metaController extends mcpBaseModelClass {
@@ -11,14 +11,14 @@ class metaController extends mcpBaseModelClass {
      *  Ideally this method shuld be used to insert the model code and the other are to be used only as normal
      */
     protected function moduleCore() {
-        /// is empty waith to be implemented 
+        /// is empty waith to be implemented
     }
 
     /**
      *  Ideally this method shuld be used to insert the model code and the other are to be used only as normal
      */
     protected function moduleSingleTon() {
-        /// is empty waith to be implemented 
+        /// is empty waith to be implemented
     }
 
 
