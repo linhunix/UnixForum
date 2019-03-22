e29333e0d58d323823bb64520b5546722f75441d
LinHUniX Andrea Morello <andrea.morello@unix.team>
2019-03-22 07:26:37 +0000

    fix config

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
 $lnxmcp_phar = array(
     "app.menu.InitCommon" => array(
         //"pdo" => array("module" => "Pdo", "type" => "serviceCommon", "input" => $scopePdo),
-        "mail" => array ("module" => "Mail", "type" => "serviceCommon") // use zend mail 
+        "mail" => array ("module" => "Mail", "type" => "serviceCommon"), // use zend mail 
+        "gfx" => array ("module" => "Gfx", "type" => "serviceCommon") // use zend mail 
     ),
 );
+//require __DIR__ . "/mcp/lnxmcp.phar";
+**exit();
 if ( file_exists( __DIR__ . "/../lnxmcp/mcp/Head.php")) {
     require __DIR__ . "/../lnxmcp/mcp/Head.php";
 } else {
-    require __DIR__ . "/dist/lnxmcp.phar";
+    require __DIR__ . "/mcp/lnxmcp.phar";
 }
 //error_log(print_r(lnxmcp(),1));
 #lnxmcp()->Block("home",array(),"UecForum");
